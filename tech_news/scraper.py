e44eb40d86b0de90fee8a42358b066a692c45769
Lívio Lopes <antonio.lvio760@gmail.com>
2023-11-12 15:59:39 -0300

    feat(scrapper): implements function fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,7 +1,20 @@
+import requests
+from time import sleep
+
+
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
-    raise NotImplementedError
+    sleep(1)
+    try:
+        response = requests.get(
+            url, timeout=3, headers={"user-agent": "Fake user-agent"}
+        )
+        if response.status_code != 200:
+            return None
+        return response.text
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
