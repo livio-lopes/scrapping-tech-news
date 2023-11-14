0233025a62293bd216a3d59157303326de251c1e
Lívio Lopes <antonio.lvio760@gmail.com>
2023-11-14 18:45:33 -0300

    feat(search_engine): implements search_by_title function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,7 +1,16 @@
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
     """Seu código deve vir aqui"""
-    raise NotImplementedError
+    query = {"title": {"$regex": title, "$options": "i"}}
+
+    result = search_news(query)
+
+    result = [(doc["title"], doc["url"]) for doc in result]
+
+    return result
 
 
 # Requisito 8
