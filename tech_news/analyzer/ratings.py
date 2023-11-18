7ae4fd56d3bcff181083cd2cceb3e273c74476f4
Lívio Lopes <antonio.lvio760@gmail.com>
2023-11-18 16:40:33 -0300

    feat(ratings): implements function top_5_catagogies

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,23 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_categories():
     """Seu código deve vir aqui"""
-    raise NotImplementedError
+
+    all_news = find_news()
+    count_categories = dict()
+    for new in all_news:
+        if new["category"] in count_categories:
+            count_categories[new["category"]] += 1
+        else:
+            count_categories.setdefault(new["category"], 1)
+    output = sorted(
+        count_categories.keys(), key=lambda x: (-count_categories[x], x)
+    )
+    print(output[:5])
+    return output[:5]
+
+
+if __name__ == "__main__":
+    print(top_5_categories())
